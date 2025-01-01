738d3344ee3c4078c9fe50950f529116cbaef529
RabeelAhmed <rabeelsulehria3@gmail.com>
2025-01-01 21:31:34 +0500

    Add location panel and vehicles descriptions.

--- frontend/src/components/LocationPanel.jsx
+++ frontend/src/components/LocationPanel.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const LocationPanel = () => {
   return (
-    <div>LocationPanel</div>
+    <div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+      <div className='flex gap-3 justify-start my-4 items-center'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>USKT, Daska road Sialkot punjab</h4>
+      </div>
+    </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -43,7 +43,7 @@ export const Home = () => {
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img
           className="w-16 absolute left-5 top-5"
           src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
@@ -83,6 +83,42 @@ export const Home = () => {
 
           <div ref={panelRef} className=' bg-white h-0'>
             <LocationPanel />
+          </div>
+        </div>
+
+        <div className='fixed w-full z-10 bottom-0 px-3 py-6 bg-white'>
+          <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
+          <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367538/assets/31/ad21b7-595c-42e8-ac53-53966b4a5fee/original/Final_Black.png" alt="" />
+
+            <div className='ml-2  w-1/2'>
+              <h4 className='font-medium text-base '>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+              <h5 className='font-medium text-sm '>2 mins away</h5>
+              <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>193.20</h2>
+          </div>
+
+          <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+
+            <div className='ml-2 w-1/2'>
+              <h4 className='font-medium text-base '>UberAuto <span><i className="ri-user-3-fill"></i>3</span></h4>
+              <h5 className='font-medium text-sm '>2 mins away</h5>
+              <p className='font-normal text-xs text-gray-600'>Affordable, Auto rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>118.86</h2>
+          </div>
+
+          <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-16' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+
+            <div className='ml-2  w-1/2'>
+              <h4 className='font-medium text-base '>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
+              <h5 className='font-medium text-sm '>3 mins away</h5>
+              <p className='font-normal text-xs text-gray-600'>Affordable, Motorcycle rides</p>
+            </div>
+            <h2 className='text-lg font-semibold'>65</h2>
           </div>
         </div>
     </div>
